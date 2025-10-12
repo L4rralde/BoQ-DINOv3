f408847b03aed113e721ae284f47869a2e3fca76
l4rralde <ealarralde@gmail.com>
2025-10-12 14:00:06 -0600

    Adding script to run experiments

---
+++ experiments.sh
@@ -0,0 +1,19 @@
+#Baseline
+echo "Running baseline with 0 trainable (dinov2) attention blocks"
+python train.py --backbone dinov2_vitb14 --epochs 5 --warmup 2
+
+#Baseline, but DINOv3
+echo "Running baseline with DINOv3 and 0 trainable attention blocks"
+python train.py --backbone dinov3_vitb16 --epochs 5 --warmup 2
+
+#DINOv3 backbone + cls token
+echo "Running with backbone DINOv3 and appending cls token"
+python train.py --backbone dinov3_vitb16 --epochs 5 --warmup 2 --cls_token
+
+#DINOv3 backbone + norm layer
+echo "Running with backbone DINOv3 with norm layer enabled"
+python train.py --backbone dinov3_vitb16 --epochs 5 --warmup 2 --norm_layer
+
+#DINOv3 backbone + cls_token + norm layer
+echo "Running with backbone DINOv3 with norm layer enabled and cls token"
+python train.py --backbone dinov3_vitb16 --epochs 5 --warmup 2 --norm_layer --cls_token
