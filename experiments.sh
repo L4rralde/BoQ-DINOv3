ee5cedff90807dbbaeeb8768726099e931878785
l4rralde <ealarralde@gmail.com>
2025-11-05 11:21:24 -0600

    Adding dinov2 + norm layer and comments to highlight best results

--- experiments.sh
+++ experiments.sh
@@ -2,6 +2,10 @@
 echo "Running baseline with 0 trainable (dinov2) attention blocks"
 python train.py --backbone dinov2_vitb14 --epochs 5 --warmup 2
 
+#Baseline with norm layer
+echo "Running baseline with 0 trainable (dinov2) attention blocks and norm layer"
+python train.py --backbone dinov2_vitb14 --epochs 5 --warmup 2 --norm_layer
+
 #Baseline, but DINOv3
 echo "Running baseline with DINOv3 and 0 trainable attention blocks"
 python train.py --backbone dinov3_vitb16 --epochs 5 --warmup 2
@@ -10,7 +14,7 @@ python train.py --backbone dinov3_vitb16 --epochs 5 --warmup 2
 echo "Running with backbone DINOv3 and appending cls token"
 python train.py --backbone dinov3_vitb16 --epochs 5 --warmup 2 --cls_token
 
-#DINOv3 backbone + norm layer
+#DINOv3 backbone + norm layer. Best BoQ with DINOv3
 echo "Running with backbone DINOv3 with norm layer enabled"
 python train.py --backbone dinov3_vitb16 --epochs 5 --warmup 2 --norm_layer
 
